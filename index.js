4fdc8d5a4d7ee09b642b7f32c1b2cdbf16ed5905
julia-francais <ju.francais@gmail.com>
2020-03-25 13:47:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+var serviceAccount = require("/Users/juliafrancais/social-ape-bf22a-firebase-adminsdk-kiawz-7ab67de76c.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://social-ape-bf22a.firebaseio.com/"
+});
+
+const express = require("express");
+const app = express();
+
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+});
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString()
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
